fc4dfd7c6140543075020f630020c58d294c6582
Coding Services <codingservices2020@gmail.com>
2026-05-21 10:05:25 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,37 +1,16 @@
-# from flask import Flask, render_template
-# from threading import Thread
-
-# app =Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     return "Alive"
-
-# def run():
-#     app.run(host='0.0.0.0', port=8080)
-
-# def keep_alive():
-#     t = Thread(target=run)
-#     t.start()
-
 from flask import Flask
 from threading import Thread
-import os
 
-app = Flask(__name__)
+app =Flask(__name__)
 
 @app.route('/')
 def index():
     return "Alive"
 
 def run():
-    port = int(os.environ.get("PORT", 10000))
-    app.run(
-        host="0.0.0.0",
-        port=port
-    )
+    app.run(host='0.0.0.0', port=8080)   # For Render port is 10000
 
 def keep_alive():
     t = Thread(target=run)
-    t.daemon = True
+    # t.daemon = True              # uncomment this for if you use above render port
     t.start()
