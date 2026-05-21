1dad4db85a8863bef126249a41a8ed4a12783a6a
Coding Services <codingservices2020@gmail.com>
2026-05-21 10:09:13 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -8,9 +8,10 @@ def index():
     return "Alive"
 
 def run():
-    app.run(host='0.0.0.0', port=8080)   # For Render port is 10000
+    # app.run(host='0.0.0.0', port=8080)
+    app.run(host='0.0.0.0', port=10000)   # For Render port is 10000
 
 def keep_alive():
     t = Thread(target=run)
-    # t.daemon = True              # uncomment this for if you use above render port
+    t.daemon = True              # uncomment this for if you use above render port
     t.start()
