f6a4fb2d7e45e58303505a8dc24482f33b56c564
Coding Services <codingservices2020@gmail.com>
2026-05-22 12:34:29 +0530

    Update firebase_db.py

--- firebase_db.py
+++ firebase_db.py
@@ -9,15 +9,15 @@ from dotenv import load_dotenv
 # Load environment variables
 load_dotenv()
 
-# DB_FILE_NAME = "testing_database"  # Define the firebase database file
-DB_FILE_NAME = "Reports_Download_links"  # Define the firebase database file
+DB_FILE_NAME = "testing_database"  # Define the firebase database file
+# DB_FILE_NAME = "Reports_Download_links"  # Define the firebase database file
 
 # Build the Firebase credentials dictionary dynamically
 firebase_config = {
     "type": os.getenv("FIREBASE_TYPE"),
     "project_id": os.getenv("FIREBASE_PROJECT_ID"),
     "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
-    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY","").replace("\\n", "\n"),
     "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
     "client_id": os.getenv("FIREBASE_CLIENT_ID"),
     "auth_uri": os.getenv("FIREBASE_AUTH_URI"),
